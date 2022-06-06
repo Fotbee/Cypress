6d23ceb5b62a69fde70cb8155720958226a6b972
Fotbee <mr.gevlich@gmail.com>
2022-06-06 22:18:44 +0300

    more tests

--- cypress/integration/project_tests/automation-test-store/contact-us.cy.js
+++ cypress/integration/project_tests/automation-test-store/contact-us.cy.js
@@ -2,25 +2,32 @@
 
 describe('Test Contact Us form via Automation test store', () => {
 
-    it('Page header should have right properties', () => {
+
+    before(function(){
+        cy.fixture('personal_information').then(function(data){
+            //this.data = data
+            globalThis.data = data
+        })
+    })
+
+    beforeEach(() => {
         cy.visit('https://automationteststore.com/')
         cy.xpath("//a[contains(@href, 'contact')]").click().then(function(linkText){
             console.log('Button text is: '+ linkText.text())
     })
+      })
+
+    it('Page header should have right properties', () => {
+
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'Contact Us')
         cy.url().should('include', 'contact')
     })
 
-
     it('Should be able to submit a successful submission via Contact Us form', () => {
-        cy.visit('https://automationteststore.com/')
-        cy.xpath("//a[contains(@href, 'contact')]").click().then(function(linkText){
-            console.log('Button text is: '+ linkText.text())
 
-        })
-        cy.get('#ContactUsFrm_first_name').type('Heval')
-        cy.get('#ContactUsFrm_email').type('test@gmal.com')
+        cy.get('#ContactUsFrm_first_name').type(data.first_name)
+        cy.get('#ContactUsFrm_email').type(data.email)
         cy.get('#ContactUsFrm_enquiry').type('text')
         cy.get("button[title='Submit']").click()
         cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
--- cypress/integration/project_tests/webdriver-uni/contact-us.cy.js
+++ cypress/integration/project_tests/webdriver-uni/contact-us.cy.js
@@ -1,16 +1,23 @@
 /// <reference types="Cypress" />
 
 describe('Test Contact Us form via WebdriverUni', () => {
+  
+  beforeEach(function(){
+    cy.fixture('userDetail').as('user')
+  })
+
   it('Should be able to submit a successful submission via Contact Us form', () => {
     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html');
     cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
     cy.title().should('contain', 'WebDriver | Contact Us');
     cy.url().should('contain', 'contactus');
     //cy.get('#contact-us').click()
-    cy.get('[name="first_name"]').type('Bohdan');
-    cy.get('[name="last_name"]').type('Hevlich');
-    cy.get('[name="email"]').type('test@gmail.com');
-    cy.get('textarea.feedback-input').type('comment');
+    cy.get('@user').then((user)=>{
+      cy.get('[name="first_name"]').type(user.first_name);
+      cy.get('[name="last_name"]').type(user.second_name);
+      cy.get('[name="email"]').type(user.email);
+      cy.get('textarea.feedback-input').type(user.comment);
+    })
     cy.get('[type="submit"]').click();
     cy.get('h1').should('have.text', 'Thank You for your Message!');
     cy.get('#contact_reply').should(
@@ -22,9 +29,11 @@ describe('Test Contact Us form via WebdriverUni', () => {
 
   it('Should not be abel submit a successful submission via Contact Us form as all fields are required', () => {
     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html');
-    cy.get('[name="first_name"]').type('Bohdan');
-    cy.get('[name="last_name"]').type('Hevlich');
-    cy.get('textarea.feedback-input').type('comment');
+    cy.get('@user').then((user)=>{
+      cy.get('[name="first_name"]').type(user.first_name);
+      cy.get('[name="last_name"]').type(user.second_name);
+      cy.get('textarea.feedback-input').type(user.comment);
+    })
     cy.get('[type="submit"]').click();
     cy.get('body').contains('Error: all fields are required');
   });
@@ -37,10 +46,12 @@ describe('Test Contact Us form via WebdriverUni', () => {
     cy.title().should('contain', 'WebDriver | Contact Us');
     cy.url().should('contain', 'contactus');
     //cy.get('#contact-us').click()
-    cy.get('[name="first_name"]').type('Bohdan');
-    cy.get('[name="last_name"]').type('Hevlich');
-    cy.get('[name="email"]').type('test@gmail.com');
-    cy.get('textarea.feedback-input').type('comment');
+    cy.get('@user').then((user)=>{
+      cy.get('[name="first_name"]').type(user.first_name);
+      cy.get('[name="last_name"]').type(user.second_name);
+      cy.get('[name="email"]').type(user.email);
+      cy.get('textarea.feedback-input').type(user.comment);
+    })
     cy.get('[type="submit"]').click();
     cy.get('h1').should('have.text', 'Thank You for your Message!');
     cy.get('#contact_reply').should(
